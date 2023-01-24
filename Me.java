5497fbc6f9f10a1ee8874874ab5050a3d3681e3b
Yorgo Homsi <yorgonews@gmail.com>
2023-01-24 20:52:03 +0200

    My name, major ande age

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me {
+    public static void main(String[] args) {
+        System.out.println("Yorgo Homsi");
+        System.out.println("18");
+        System.out.println("Electrical Engineering"); 
+    }
+}
